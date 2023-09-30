71e1f4c6b3f8a9961d0ad367104f7fd9a44d4cf1
Jarod Santiago Viquez Salazar <jarod281@hotmail.com>
2023-09-30 15:48:46 -0600

    Semana 2

---
+++ Web01/Web01/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web01.Models;
+
+namespace Web01.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Web01/Web01/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web01.Models
+{
+    public class Usuario
+    {
+        public string consultarNombre() {
+            return "Jarod";
+                }
+    }
+}
