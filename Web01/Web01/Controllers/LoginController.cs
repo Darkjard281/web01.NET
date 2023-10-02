53f15c0b90703b6dfa2a685f12f50b58587dc024
Jarod Santiago Viquez Salazar <jarod281@hotmail.com>
2023-10-02 14:00:45 -0600

    Implementacion de Razor

--- Web01/Web01/Controllers/LoginController.cs
+++ Web01/Web01/Controllers/LoginController.cs
@@ -3,30 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web01.Entities;
 using Web01.Models;
 
 namespace Web01.Controllers
 {
     public class LoginController : Controller
     {
+
+        UsuarioModelo usuarioModelo = new UsuarioModelo();
         
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Login(UsuarioEnt entidad)
+        {
+            usuarioModelo.Login(entidad);
+            return View();
+        }
+
+        [HttpGet]
         public ActionResult RegistrarCuenta()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult RegistrarCuenta(UsuarioEnt entidad)
+        {
+            usuarioModelo.RegistrarCuenta(entidad);
+            return View();
+        }
 
+        [HttpGet]
         public ActionResult RecuperarCuenta()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RecuperarCuenta(UsuarioEnt entidad)
+        {
+            usuarioModelo.RecuperarCuenta(entidad);
             return View();
         }
 
--- Web01/Web01/Models/UsuarioModelo.cs
+++ Web01/Web01/Models/UsuarioModelo.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web01.Entities;
 
 namespace Web01.Models
 {
     public class UsuarioModelo
     {
 
-        public void Login()
+        public void Login(UsuarioEnt entidad)
         {
        
         }
 
-        public void RegistrarCuenta()
+        public void RegistrarCuenta(UsuarioEnt entidad)
         {
        
         }
 
-        public void RecuperarCuenta()
+        public void RecuperarCuenta(UsuarioEnt entidad)
         {
         
         }
