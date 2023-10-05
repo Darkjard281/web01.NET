5b0c60b1eb931a13608f7734d8fbe76709d8b74e
Jarod Santiago Viquez Salazar <jarod281@hotmail.com>
2023-10-04 22:28:31 -0600

    Semana 4

---
+++ APIWEB01/APIWEB01/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APIWEB01.Entities;//Agregamos la referencia de la carpeta
+
+namespace APIWEB01.Controllers
+{
+    public class LoginController : ApiController
+    {
+
+        [HttpPost]// Se recibe el objeto ya que si pasamos por parametro mediante el metodo get queda expuesta la informacion
+        [Route("Login")]//Parametro obligatorio para darle un alias al metodo
+        public void Login(UsuarioEnt entidad) //objeto / alias
+        {
+
+        }
+
+        [HttpPost]
+        [Route("RegistrarCuenta")]
+        public void RegistrarCuenta(UsuarioEnt entidad)
+        {
+
+        }
+
+        public void RecuperarCuenta(UsuarioEnt entidad)
+        {
+
+        }
+
+    }
+}
---
+++ APIWEB01/APIWEB01/Entities/UsuarioEnt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIWEB01.Entities
+{
+    public class UsuarioEnt
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+
+    }
+}
